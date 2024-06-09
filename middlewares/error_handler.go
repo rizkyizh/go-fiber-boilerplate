11a54ae917817fa6464b927d85dc4e6af9f9e5c6
rizkyizh <rizkiizzulhaq14@gmail.com>
2024-06-09 22:38:34 +0700

    fix: error handler middleware

--- middlewares/error_handler.go
+++ middlewares/error_handler.go
@@ -1,6 +1,7 @@
 package middlewares
 
 import (
+	"encoding/json"
 	"reflect"
 
 	"github.com/go-playground/validator/v10"
@@ -11,12 +12,33 @@ import (
 
 func ValidateRequest(reqBody interface{}) fiber.Handler {
 	return func(c *fiber.Ctx) error {
+		errHandler := &utils.ResponseHandler{}
+
+		// Decode request body into a map
+		var bodyMap map[string]interface{}
+		if err := json.Unmarshal(c.Body(), &bodyMap); err != nil {
+			return errHandler.BadRequest(c, []string{"Invalid request body"})
+		}
+
 		typ := reflect.TypeOf(reqBody).Elem()
 		v := reflect.New(typ).Interface()
-		errHandler := &utils.ResponseHandler{}
 
 		if err := c.BodyParser(v); err != nil {
 			return errHandler.BadRequest(c, []string{"Cannot parse JSON"})
+		}
+
+		// Create a set of allowed fields from the DTO
+		allowedFields := make(map[string]struct{})
+		value := reflect.ValueOf(reqBody).Elem()
+		for i := 0; i < value.NumField(); i++ {
+			allowedFields[value.Type().Field(i).Tag.Get("json")] = struct{}{}
+		}
+
+		// Check for unexpected fields
+		for field := range bodyMap {
+			if _, found := allowedFields[field]; !found {
+				return errHandler.BadRequest(c, []string{"Unexpected field: " + field})
+			}
 		}
 
 		if err := utils.ValidateStruct(v); err != nil {
